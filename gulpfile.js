0e32e5028cb58db8b662c1aafc619ee04d50377c
felics <fspoettel@gmail.com>
2014-01-14 14:31:38 +0100

    Update to build-process, fuck livereload

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,12 @@
 // Require plugins
-var lr           = require('tiny-lr'),
-    gulp         = require('gulp'),
+var gulp         = require('gulp'),
     rename       = require("gulp-rename"),
     concat       = require('gulp-concat'),
     uglify       = require('gulp-uglify'),
     sass         = require('gulp-ruby-sass'),
     prefix       = require('gulp-autoprefixer'),
     cssmin       = require('gulp-minify-css'),
-    imagemin     = require ('gulp-imagemin'),
-    livereload   = require('gulp-livereload'),
-    server       = lr();
+    imagemin     = require ('gulp-imagemin');
 
 // Concat & Minify JS
 gulp.task('scripts', function(){
@@ -19,13 +16,12 @@ gulp.task('scripts', function(){
         .pipe(uglify())
         .pipe(rename('global.min.js'))
         .pipe(gulp.dest('js/build'))
-        .pipe(livereload(server));
 });
 
 // Build, autoprefix & minify CSS
 gulp.task('sass', function () {
     gulp.src('css/build.scss')
-        .pipe(sass({noCache:true,quiet:true}))
+        .pipe(sass({noCache:true,debugInfo:true}))
         .pipe(rename('global.css'))
         .pipe(gulp.dest('css/build/unprefixed'))
         .pipe(prefix("last 2 versions", "> 1%", "ie 8"))
@@ -33,7 +29,6 @@ gulp.task('sass', function () {
         .pipe(cssmin())
         .pipe(rename('global.min.css'))
         .pipe(gulp.dest('css/build/prefixed'))
-        .pipe(livereload(server));
 });
 
 // Minify images
@@ -41,26 +36,12 @@ gulp.task('images', function () {
     gulp.src('img/*')
         .pipe(imagemin())
         .pipe(gulp.dest('img'))
-        .pipe(livereload(server));
-});
-
-// Reload when HTML 
-gulp.task('html', function () {
-    gulp.src(['templates/**','layouts/**','partials/**'])
-        .pipe(livereload(server));
-});
-
-// Run Server
-gulp.task('livereload', function(){  
-    server.listen(35729, function(err){
-        if(err) return console.log(err);
-    });
 });
 
 // Default
 gulp.task('default', function(){
     // Run Task
-    gulp.run('scripts', 'sass', 'images','html', 'livereload');
+    gulp.run('scripts', 'sass', 'images');
 
     // Watch & build .scss
     gulp.watch('css/**/*.scss' , function(ev){
@@ -68,17 +49,12 @@ gulp.task('default', function(){
     });
 
     // Watch & build js
-    gulp.watch(['js/global.js', 'js/lib/plugins/**'], function(ev){
+    gulp.watch(['js/global.js','js/user/*.js', 'js/lib/plugins/*.js'], function(ev){
         gulp.run('scripts')
     });
 
     // Watch & minify images
     gulp.watch(['img/**'], function(ev){
         gulp.run('images')
-    });
-
-    // Watch & reload HTML (needs testing)
-    gulp.watch(['templates/**','layouts/**','partials/**'], function(ev){
-        gulp.run('html')
     });
 });
