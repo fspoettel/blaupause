35b9bcaae44b980845cb75999e246a77e311d36b
felics <fspoettel@gmail.com>
2014-01-17 23:18:59 +0100

    Codebase cleanup
     - Consistent quote style
     - Changed ‚layouts‘ to ‚layout‘

--- gulpfile.js
+++ gulpfile.js
@@ -1,64 +1,64 @@
 // Require plugins
-var gulp         = require('gulp'),
+var gulp         = require("gulp"),
     rename       = require("gulp-rename"),
-    concat       = require('gulp-concat'),
-    uglify       = require('gulp-uglify'),
-    sass         = require('gulp-ruby-sass'),
-    prefix       = require('gulp-autoprefixer'),
-    cssmin       = require('gulp-minify-css'),
-    plumber      = require('gulp-plumber'),
-    imagemin     = require ('gulp-imagemin');
+    concat       = require("gulp-concat"),
+    uglify       = require("gulp-uglify"),
+    sass         = require("gulp-ruby-sass"),
+    prefix       = require("gulp-autoprefixer"),
+    cssmin       = require("gulp-minify-css"),
+    plumber      = require("gulp-plumber"),
+    imagemin     = require ("gulp-imagemin");
 
 // Concat & Minify JS
-gulp.task('scripts', function(){
-    gulp.src(['js/lib/plugins/*.js','js/user/*.js', 'js/global.js'])
-        .pipe(concat('global.js'))
-        .pipe(gulp.dest('js/build'))
+gulp.task("scripts", function(){
+    gulp.src(["js/lib/plugins/*.js","js/user/*.js", "js/global.js"])
+        .pipe(concat("global.js"))
+        .pipe(gulp.dest("js/build"))
         .pipe(uglify())
-        .pipe(rename('global.min.js'))
-        .pipe(gulp.dest('js/build'))
+        .pipe(rename("global.min.js"))
+        .pipe(gulp.dest("js/build"))
 });
 
 // Build, autoprefix & minify CSS
-gulp.task('sass', function () {
-    gulp.src('css/build.scss')
+gulp.task("sass", function () {
+    gulp.src("css/build.scss")
         .pipe(plumber())
         .pipe(sass({noCache:true}))
-        .pipe(rename('global.css'))
-        .pipe(gulp.dest('css/build/unprefixed'))
-        .pipe(prefix("last 2 versions", "> 1%", "ie 8"))
-        .pipe(gulp.dest('css/build/prefixed'))
+        .pipe(rename("global.css"))
+        .pipe(gulp.dest("css/build/unprefixed"))
+        .pipe(prefix("last 2 versions", "> 1%"))
+        .pipe(gulp.dest("css/build/prefixed"))
         .pipe(cssmin())
-        .pipe(rename('global.min.css'))
-        .pipe(gulp.dest('css/build/prefixed'))
+        .pipe(rename("global.min.css"))
+        .pipe(gulp.dest("css/build/prefixed"))
 });
 
 // Minify images
-gulp.task('images', function () {
-    gulp.src('img/src/*')
+gulp.task("images", function () {
+    gulp.src("img/src/*")
         .pipe(imagemin())
-        .pipe(gulp.dest('img/opti'))
+        .pipe(gulp.dest("img/opti"))
 });
 
 //
 
 // Default
-gulp.task('default', function(){
+gulp.task("default", function(){
     // Run Task
-    gulp.run('scripts', 'sass', 'images');
+    gulp.run("scripts", "sass", "images");
 
     // Watch & build .scss
-    gulp.watch('css/**/*.scss' , function(ev){
-        gulp.run('sass')
+    gulp.watch("css/**/*.scss" , function(ev){
+        gulp.run("sass")
     });
 
     // Watch & build js
-    gulp.watch(['js/global.js', 'js/lib/plugins/*.js', 'js/user/*.js'], function(ev){
-        gulp.run('scripts')
+    gulp.watch(["js/global.js", "js/lib/plugins/*.js", "js/user/*.js"], function(ev){
+        gulp.run("scripts")
     });
 
     // Watch & minify images
-    gulp.watch(['img/src/*'], function(ev){
-        gulp.run('images')
+    gulp.watch(["img/src/*"], function(ev){
+        gulp.run("images")
     });
 });
