62600670357316e17f27d5989265bf0833a1aa33
felics <fspoettel@gmail.com>
2015-02-23 19:02:38 +0100

    Add array-support for config.scripts.src to "hint"-task
    Closes #38

--- gulp/tasks/hint.js
+++ gulp/tasks/hint.js
@@ -10,7 +10,16 @@ var gulp    = require("gulp"),
     config  = require("../config").scripts;
 
 gulp.task("hint", function(){
-  return gulp.src([config.entry, config.src])
+
+  var toHint = [];
+
+  toHint.push(config.entry)
+
+  for(var i = 0; i < config.watch.length; i++){
+    toHint.push(config.watch[i])
+  }
+
+  return gulp.src(toHint)
     .pipe(jshint())
     .pipe(jshint.reporter(stylish))
 });
