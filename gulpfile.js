05bd6b4f45bc7bdc15929d55447b61489866de89
Felix S <felix.spoettel@fu-berlin.de>
2015-11-02 13:49:29 +0100

    Add CL-Output for production-flag

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,14 @@
 
 'use strict';
 
+const argv       = require('yargs').argv;
+const chalk      = require('chalk');
 const requireDir = require('require-dir');
+
+const isProduction =  (argv.production || argv.p);
+
+if (isProduction) {
+  console.log(chalk.bold.bgGreen('Production Mode'));
+}
 
 requireDir('./gulp/tasks', { recurse: true });
