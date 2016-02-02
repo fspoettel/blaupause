d29921435a2ebc8c72346e1278ed0ca24f809337
Felix Spöttel <contact@felics.me>
2016-02-02 12:56:49 +0100

    :wrench: Use template strings in gulp-config

--- gulp/config.js
+++ gulp/config.js
@@ -17,24 +17,24 @@ module.exports = {
     /** Port for browserSync-proxy */
     port: 4000,
     /** Use 'proxy' to reload existing servers */
-    proxy: hostname + ':' + port
+    proxy: `${hostname}:${port}`
   },
 
   fonts: {
-    src: src + '/fonts/**/*.{ttf,woff,woff2}',
-    dest: dest + '/css/fonts',
+    src: `${src}/fonts/*.{ttf,woff,woff2}`,
+    dest: `${dest}/css/fonts`
   },
 
   images: {
-    dest: dest + '/img',
+    src: `${src}/img/*`,
+    dest: `${dest}/img`,
     settings: {
-      progressive: true,
-    },
-    src: src + '/img/*',
+      progressive: true
+    }
   },
 
   modernizr: {
-    dest: dest + '/js/vendor',
+    dest: `${dest}/js/vendor`,
     settings: {
       options: [
         'setClasses',
@@ -43,48 +43,48 @@ module.exports = {
         'testProp',
         'fnBind',
       ],
-      tests: [],
-    },
+      tests: []
+    }
   },
 
   scripts: {
-    bundles: [src + '/js/*.js'],
-    dest: dest + '/js',
+    bundles: [`${src}/js/*.js`],
+    dest: `${dest}/js`,
+    watch: [`${src}/js/**/*.{js,jsx}`],
     externals: {
-      jquery: 'jQuery',
-    },
-    watch: [src + '/js/**/*.{js,jsx}'],
+      jquery: 'jQuery'
+    }
   },
 
   server: {
     /** Disable if you don't want to run an Express-server (e.g. use MAMP) */
     run: true,
+    file: 'server.js',
     /** Needed for clean task & server */
     dest,
-    file: 'server.js',
     port,
     /** Needed for BrowserSync to work properly with server restarts */
     reloadDelay: 500,
     /** Tell nodemon which directories/files to watch */
-    watch: ['server.js'],
+    watch: ['server.js']
   },
 
   styles: {
+    src: `${src}/scss/*.{sass,scss}`,
+    dest: `${dest}/css`,
+    watch: [`${src}/scss/**/*.{sass,scss}`],
     autoprefixer: {
-      browsers: ['last 2 versions', 'ie >= 10', 'Android >= 4.4'],
-    },
-    dest: dest + '/css',
-    src: src + '/scss/*.{sass,scss}',
-    watch: [src + '/scss/**/*.{sass,scss}'],
+      browsers: ['last 2 versions', 'ie >= 10', 'Android >= 4.4']
+    }
   },
 
   svg: {
-    src: src + '/img/svg/**/*.svg',
-    dest: dest + '/img/svg'
+    src: `${src}/img/svg/**/*.svg`,
+    dest: `${dest}/img/svg`
   },
 
   views: {
     dest,
-    index: src + '/*.*',
-  },
+    index: `${src}/*`
+  }
 };
