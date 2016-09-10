2840f40a7178de84bf03868a8bc5c3d4cb10659d
Felix Spöttel <hi@felics.me>
2016-09-10 13:29:46 +0200

    :wrench: Add automatic travis deployment
    - Deploy gh-pages automatically after push to master
    - Move linting to npm pretest
    - Minimal styles for built page
    - Update dependencies

---
+++ internals/scripts/deploy.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+set -o errexit -o nounset
+
+# Do nothing if the commit was not pushed to master
+if [ "$TRAVIS_PULL_REQUEST" != "false" ] || [ "$TRAVIS_BRANCH" != "master" ]
+then
+  echo "This commit was made inside a pull request or against the $TRAVIS_BRANCH and not the master! No deploy!"
+  exit 0
+fi
+
+#  Set Travis as commiter git user
+git config --global user.email "travis-ci@localhost"
+git config --global user.name "Travis CI"
+
+# Rebuild docs with webpack
+gulp -p
+
+# Initialize an empty repository, fetch upstream and reset it
+rev=$(git rev-parse --short HEAD)
+cd ./public
+git init
+
+# Add all changes in push them to gh-pages
+touch .
+git add -A .
+git commit -m "rebuild pages at ${rev}"
+git push --force --quiet "https://${GITHUB_TOKEN}@github.com/${GITHUB_REPO}.git" master:gh-pages > /dev/null 2>&1
