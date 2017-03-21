0ef79b5ed21c185c0a65406b2c4b5968c4f05f55
Felix Spöttel <hi@felics.me>
2017-03-21 18:55:16 +0100

    Replace ava with Jest

--- internals/test/helpers/setup-browser-env.js
+++
@@ -1,6 +0,0 @@
-process.env.NODE_ENV = 'test';
-
-global.document = require('jsdom').jsdom('<html><body></body></html>');
-
-global.window = document.defaultView;
-global.navigator = window.navigator;
