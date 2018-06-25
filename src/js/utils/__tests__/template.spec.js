e58da46a948ece5d711e586c2801fb3ef57dd664
Felix Spöttel <hi@felics.me>
2018-06-25 23:09:23 +0200

    Update linter, update lockfile

--- src/js/utils/__tests__/template.spec.js
+++ src/js/utils/__tests__/template.spec.js
@@ -7,10 +7,10 @@ function isElement(o) {
 function isNodeList(nodes) {
   const stringRepr = Object.prototype.toString.call(nodes);
 
-  return typeof nodes === 'object' &&
-    /^\[object (HTMLCollection|NodeList|Object)\]$/.test(stringRepr) &&
-    (typeof nodes.length === 'number') &&
-    (nodes.length === 0 || (typeof nodes[0] === 'object' && nodes[0].nodeType > 0));
+  return typeof nodes === 'object'
+  && /^\[object (HTMLCollection|NodeList|Object)\]$/.test(stringRepr)
+  && (typeof nodes.length === 'number')
+  && (nodes.length === 0 || (typeof nodes[0] === 'object' && nodes[0].nodeType > 0));
 }
 
 describe('htmlToElement()', () => {
